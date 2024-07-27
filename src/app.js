c3dd12918e13a5628d17de460e7b9eacf6bb012e
github@syedsarkheel <syedsarkheelbaseer@gmail.com>
2024-07-27 15:10:37 +0530

    updated app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import express from "express";
 import { indexRouter } from "./routes/index.route.js";
+import { ApiResponse } from "./utils/ApiResponse.js";
 
 const app = express();
 
 app.use(express.json()); //Body Parser
 
 app.use("/", indexRouter);
+
+app.use("/", (req, res, next) => {
+  return res
+    .status(404)
+    .send(new ApiResponse(404, null, "No such route exists"));
+});
 
 export { app };
